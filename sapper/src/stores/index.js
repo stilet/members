3202085989acc18dcb6d4ba43ce0926805d17728
Dexter <dexter@dexterlabs.nl>
2019-04-13 16:13:00 +0200

    Need to switch to rollup since webpack does't work

--- sapper/src/stores/index.js
+++ sapper/src/stores/index.js
@@ -1,26 +1,26 @@
+// Store rewrite to v3
 
+// import { Store as BaseStore } from 'svelte/store.js';
 
-import { Store as BaseStore } from 'svelte/store.js';
+// import graphqlStore from '@dxlb/svelte-graphql-store'
+// import membersStore from './members'
+// import authStore from './auth'
+// import notificationStore from './notifications'
 
-import graphqlStore from '@dxlb/svelte-graphql-store'
-import membersStore from './members'
-import authStore from './auth'
-import notificationStore from './notifications'
+// function chain(first, ...rest) {
+//   if (rest.length) {
+//     return store => chain(...rest)(first(store))
+//   } else {
+//     return store => first(store)
+//   }
+// }
 
-function chain(first, ...rest) {
-  if (rest.length) {
-    return store => chain(...rest)(first(store))
-  } else {
-    return store => first(store)
-  }
-}
-
-export default function createStore(data) {
-  const Store = chain(
-    graphqlStore,
-    membersStore,
-    authStore,
-    notificationStore
-  )(BaseStore)
-  return new Store(data)
-}
+// export default function createStore(data) {
+//   const Store = chain(
+//     graphqlStore,
+//     membersStore,
+//     authStore,
+//     notificationStore
+//   )(BaseStore)
+//   return new Store(data)
+// }
